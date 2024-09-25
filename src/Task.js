4ffb1ef9e48400a9a481f47ae7fb4ac0fc019ed9
Jayden Hendrickse <jayden.heberhendrickse@gmail.com>
2024-09-25 18:20:02 +0200

    Started building ToDoList logic. Will refactor to not export a single obkect for no reason

--- src/Task.js
+++ src/Task.js
@@ -2,8 +2,8 @@ export default function Task(title, description, dueDate, priority) {
     //constructor
     this.title = title;
     this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
+    this.dueDate = dueDate; // Date object
+    this.priority = priority; // int
 
     //TODO:Accessor methods maybe?
     
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,22 @@
 // Contains the logic for managing projects and tasks
+import Task from "./Task";
+import Project from "./Project";
 
 //TODO:Edit Tasks
 //TODO:Edit Projects
+//TODO:Add lists to Local Storage when creating or adding
+
+export default function TodoList(){
+    const today = new Date();
+
+    this.taskList = [];
+    this.projectList = [];
+
 //TODO:Create "Today" project
+    const todayTasks = this.taskList.filter(task => isDueToday(task.dueDate));
+}
+
+function isDueToday(dueDate){
+    let today = new Date();
+    return dueDate.setHours(0,0,0,0) == today.setDate();
+}
