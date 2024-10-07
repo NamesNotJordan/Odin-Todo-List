8d5f290afa6bdbf44ef3d3be0fd77f504b87a132
Jayden Hendrickse <jayden.heberhendrickse@gmail.com>
2024-10-07 19:48:47 +0200

    added localStorage methods

--- src/TodoList.js
+++ src/TodoList.js
@@ -11,10 +11,26 @@ const today = new Date();
 taskList = [];
 projectList = [];
 
-const todayTasks = this.taskList.filter(task => isDueToday(task.dueDate));
+const todayTasks = this.taskList.filter(task => isDueToday(Date(task.dueDate)));
 
 
 function isDueToday(dueDate){
     let today = new Date();
     return dueDate.setHours(0,0,0,0) == today.setHours(0,0,0,0);
+}
+function loadLists() {
+    if (localStorage.getItem("tasks")) {
+        taskList = localStorage.getItem("tasks");
+    }
+    if (localStorage.getItem("projects")) {
+        projectList = localStorage.getItem("projects")
+    }
+}
+function saveToStorage() {
+    localStorage.setItem("tasks", taskList);
+    localStorage.setItem("projects", projectList);
+}
+function addTaskToList(task) {
+    taskList.add(task);
+    saveToStorage();
 }
