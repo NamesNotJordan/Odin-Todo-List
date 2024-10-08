5c3b2b1a1587e73f977162e38a5ab7c3b9fc3fb1
Jayden Hendrickse <jayden.heberhendrickse@gmail.com>
2024-10-08 18:48:32 +0200

    changing TodoList to a class

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,33 +1,35 @@
 // Contains the logic for managing projects and tasks
 import Task from "./Task";
 import Project from "./Project";
+import { isToday } from "date-fns";
 
 //TODO:Edit Tasks
 //TODO:Edit Projects
 //TODO:Add lists to Local Storage when creating or adding
 
+export default class TodoList {
+    projectList = [new Project("Inbox")];
 
-const inboxProject = new Project("Inbox", "All your loose tasks");
-projectList = [inboxProject];
-
-
-
-function isDueToday(dueDate){
-    let today = new Date();
-    return dueDate.setHours(0,0,0,0) == today.setHours(0,0,0,0);
-}
-function loadProjects() {
-    if (localStorage.getItem("projects")) {
-        projectList = localStorage.getItem("projects")
+    loadProjects() {
+        if (localStorage.getItem("projects")) {
+            projectList = localStorage.getItem("projects")
+        }
     }
-}
-function saveToStorage() {
-    localStorage.setItem("projects", projectList);
-}
-function addTaskToList(task) {
-    saveToStorage();
-}
+    saveToStorage() {
+        localStorage.setItem("projects", projectList);
+    }
+    
+    getProject(projectName){
+        index = this.projectList.find(p => p.getName = projectName);
+        return this.projectList[index];
+    }
 
-function deleteProject(targetProject){ 
-    projectList = projectList.filter(p => p!== targetProject);
+    deleteProject(targetProject){ 
+        projectList = projectList.filter(p => p !== targetProject);
+    }
+
+    getTodaysTasks(){
+        
+    }
+
 }
