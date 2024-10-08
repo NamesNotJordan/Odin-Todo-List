d33ae84f7f0543bc12a8132bc53bf66e939a86e6
Jayden Hendrickse <jayden.heberhendrickse@gmail.com>
2024-10-08 23:08:45 +0200

    basic dom element creation methods

--- src/UI.js
+++ src/UI.js
@@ -1,6 +1,35 @@
-//TODO:find better name
+import TodoList from "./TodoList";
+import Project from "./Project";
+import Task from "./Task";
+import {format} from "date-fns";
 
 //Functionality:
 //TODO:View all projects
 //TODO:View all tasks in a project
 //TODO:Exapnd task for more details
+
+export default class UI {
+    TodoList = new TodoList();
+    // Load content
+
+
+    // Create Content
+    static createTaskCard(task){
+        let taskList = document.querySelector(".task-list");
+        let taskCard = document.createElement("li");
+        taskCard.classList.add("task","cyber-card");
+        taskList.appendChild(taskCard);
+
+        let taskLabel = document.createElement("p");
+        taskLabel.classList.add("task-name");
+        taskLabel.innerHTML = task.getName();
+        taskCard.appendChild(taskLabel);
+
+        let 
+        
+    }
+
+    static createProjectCard(project) {}
+
+    // Event Listeners
+}
